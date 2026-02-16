b952f88f878d446414ff49f2610599d2cf3f08bb
dev_anq <n88287101@gmail.com>
2026-02-16 20:18:52 +0200

    First GUI version, day 16

--- cli.py
+++ cli.py
@@ -2,7 +2,6 @@ from modules import functions
 import time
 
 now = time.strftime("%b %d, %Y %H:%M:%S")
-print("The time is below:")
 print("It is", now)
 
 while True:
---
+++ gui.py
@@ -0,0 +1,11 @@
+from modules import functions
+import FreeSimpleGUI as sg
+
+label = sg.Text("Напишіть задачу:")
+input_box = sg.InputText(tooltip="Впешіть задачу")
+add_botton = sg.Button("Додати")
+
+window = sg.Window('My To-do App', layout=[[label] ,[input_box, add_botton]])
+window.read()
+window.close()
+
