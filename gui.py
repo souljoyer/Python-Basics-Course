94e2a0f97e9758b8f490ca4783ff06b9fe39d571
anq(linux) <n88287101@gmail.com>
2026-03-03 19:56:19 +0200

    Кнопка "Редагувати"

--- gui.py
+++ gui.py
@@ -1,24 +1,41 @@
-import FreeSimpleGUI as sg
 import functions
+import FreeSimpleGUI as sg
 
 label = sg.Text("Напишіть задачу:")
-input_box = sg.InputText(tooltip="Впешіть задачу", key="Завдання")
-add_botton = sg.Button("Додати")
+input_box = sg.InputText(tooltip="Впешіть задачу", key="todo")
+add_botton = sg.Button("Add")
+list_box = sg.Listbox(values=functions.get_todos(), key='todos',
+                         enable_events=True, size=[45,13])
+edit_button = sg.Button("Edit")
 
 window = sg.Window('My To-do App',
-                   layout=[[label] ,[input_box, add_botton]],
-                   font=('Helvetica', 20))
+                   layout=[[label] ,[input_box, add_botton], [list_box, edit_button]],
+                   font=('Helvetica', 13))
 
 while True:
     event, values = window.read()
-    print(event)
-    print(values)
+    print(1, event)
+    print(2, values)
+    print(values['todos'])
     match event:
         case "Add":
             todos = functions.get_todos()
             new_todo = values['todo'] + "\n"
             todos.append(new_todo)
             functions.write_todos(todos)
+            window['todos'].update(values=todos)
+
+        case "Edit":
+            todo_to_edit = values['todos'][0]
+            new_todo = values['todo']
+
+            todos = functions.get_todos()
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo
+            functions.write_todos(todos, '\n')
+            window['todos'].update(values=todos)
+        case 'todos':
+            window['todos'].update(values=values['todos'])
         case sg.WIN_CLOSED:
             break
 
