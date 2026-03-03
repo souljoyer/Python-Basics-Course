0ae1f86a9308690691bbb5a3988acbdb40e5de52
anq(linux) <n88287101@gmail.com>
2026-03-03 17:03:26 +0200

    Додав кнопку "Додати", та виправив помилку з виходом із програми.

--- gui.py
+++ gui.py
@@ -1,10 +1,26 @@
 import FreeSimpleGUI as sg
+import functions
 
 label = sg.Text("Напишіть задачу:")
-input_box = sg.InputText(tooltip="Впешіть задачу")
+input_box = sg.InputText(tooltip="Впешіть задачу", key="Завдання")
 add_botton = sg.Button("Додати")
 
-window = sg.Window('My To-do App', layout=[[label] ,[input_box, add_botton]])
-window.read()
+window = sg.Window('My To-do App',
+                   layout=[[label] ,[input_box, add_botton]],
+                   font=('Helvetica', 20))
+
+while True:
+    event, values = window.read()
+    print(event)
+    print(values)
+    match event:
+        case "Add":
+            todos = functions.get_todos()
+            new_todo = values['todo'] + "\n"
+            todos.append(new_todo)
+            functions.write_todos(todos)
+        case sg.WIN_CLOSED:
+            break
+
 window.close()
 
