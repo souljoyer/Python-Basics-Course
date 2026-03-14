1d2bef93660e04992ad030e6b8b54d609cdb0e6c
anq(linux) <n88287101@gmail.com>
2026-03-14 20:47:33 +0200

    Stable build

--- web.py
+++ web.py
@@ -15,12 +15,14 @@ st.title("My Todo App")
 st.subheader("This is my first app")
 st.write("This app is is to increase your progress")
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
 
 st.text_input(label="", placeholder="Add new todo...",
               on_change=add_todo, key='new_todo')
-
-st.session_state
 
 
